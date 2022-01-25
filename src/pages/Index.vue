3a7c819d301d1123b6692ec1deb046a169e94ea8
Tai Duc Nguyen <tai.nguyen@niteco.se>
2022-01-25 10:36:24 +0700

    update layout

--- src/pages/Index.vue
+++ src/pages/Index.vue
@@ -1,13 +1,75 @@
 <template>
-  <q-page class="flex flex-center">
-    <img alt="Quasar logo" src="~assets/quasar-logo-full.svg">
-  </q-page>
+  <main>
+    <h1 class="title">Measure page quality</h1>
+
+    <p class="description">
+      Test your pages in a lab environment powered by PageSpeed Insights. Then
+      get tips and recommendations to improve your user experience. For field
+      performance, see the
+      <a
+        target="_blank"
+        href="https://pagespeed.web.dev/?utm_source=psi&utm_medium=redirect"
+        >PageSpeed Insights</a
+      >
+      tool.
+    </p>
+
+    <img alt="Tesing" width="400" height="400" src="~assets/testing.jpg" />
+  </main>
 </template>
 
 <script>
-import { defineComponent } from 'vue'
+import { defineComponent } from "vue";
 
 export default defineComponent({
-  name: 'PageIndex'
-})
+  name: "PageIndex",
+});
 </script>
+
+<style scoped>
+.container {
+  min-height: 100vh;
+  padding: 0 0.5rem;
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+}
+
+main {
+  padding: 5rem 0;
+  flex: 1;
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+}
+
+.title {
+  margin: 0;
+  line-height: 1.15;
+  font-size: 4rem;
+}
+
+.title,
+.description {
+  text-align: center;
+}
+
+.description {
+  line-height: 1.5;
+  font-size: 1.5rem;
+}
+</style>
+<style>
+html,
+body {
+  padding: 0;
+  margin: 0;
+  font-family: -apple-system, BlinkMacSystemFont, Segoe UI, Roboto, Oxygen, Ubuntu, Cantarell, Fira Sans, Droid Sans, Helvetica Neue, sans-serif;
+}
+
+* {
+  box-sizing: border-box;
+}
+</style>
